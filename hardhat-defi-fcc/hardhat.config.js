49ae8d726f54cf05504f80687f0acbd554bf0cec
su-adam <su-adam@outlook.com>
2022-07-21 17:33:19 +0100

    inital commit

---
+++ hardhat-defi-fcc/hardhat.config.js
@@ -0,0 +1,72 @@
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-gas-reporter")
+require("@nomiclabs/hardhat-etherscan")
+require("dotenv").config()
+require("solidity-coverage")
+require("hardhat-deploy")
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            forking: {
+              url: MAINNET_RPC_URL
+            },
+        },
+        rinkeby: {
+            chainId: 4,
+            blockConfirmations: 6,
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+        },
+    },
+
+    solidity: {
+      compilers: [
+          {
+              version: "0.8.8",
+          },
+          {
+              version: "0.6.12",
+          },
+          {
+              version: "0.4.19",
+          },
+          {
+            version: "0.6.6",
+          },
+          {
+            version: "0.6.0",
+          },
+      ],
+    },
+
+    etherscan: {
+        // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+        apiKey: {
+            rinkeby: ETHERSCAN_API_KEY,
+            kovan: ETHERSCAN_API_KEY,
+        },
+    },
+    gasReporter: {
+        enabled: false,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        // coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+
+    namedAccounts: {
+      deployer: {
+          default: 0, // here this will by default take the first account as deployer
+          1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+      },
+  },
+}
