bd71a050fd00e4634778565d96941b968fb8d7d6
su-adam <su-adam@outlook.com>
2022-08-09 09:46:19 +0100

    first commit

---
+++ hardhat-nft-marketplace-fcc/deploy/02-deploy-basic-nft.js
@@ -0,0 +1,24 @@
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    const args = []
+    const basicNft = await deploy("BasicNft", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfitmations: network.config.blockConfirmations || 1,
+    })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(basicNft.address, args)
+    }
+    log("-------------------------------------")
+
+}
+module.exports.tags = ["all", "basicNft"]
